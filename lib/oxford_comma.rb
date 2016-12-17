4a53c816dfa14561a01b9c3e8583081b5c33a4fa
shereen messi <shereenmessi@hotmail.com>
2016-12-17 19:18:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.size==1
+    array[0]
+  elsif array.size==2
+   array.join(" and ")
+  else
+    last=array.pop
+    str= array.join(", ")
+    str+=", and "+last
+    str
 
+
+  end
 end
